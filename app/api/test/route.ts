5a7cc607690f5598d28f53e2014e9c21145d7e74
ValenSecundaria <gemetrov@gmail.com>
2025-07-22 13:34:34 -0300

    prueba

---
+++ app/api/test/route.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const prisma = new PrismaClient()
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const estados = await prisma.estado_pedido.findMany()
+    res.status(200).json(estados)
+  } catch (error) {
+    res.status(500).json({ error: String(error) })
+  }
+}
